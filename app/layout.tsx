c27c58d93b7a80bcd03aef8ffc8f9b545b1ded7a
karolciurej <101570801+karolciurej@users.noreply.github.com>
2024-03-13 08:35:43 +0100

    chenged loginform tu client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
-        <Analytics />
+        {/* <Analytics /> */}
         <SpeedInsights />
       </body>
     </html>
